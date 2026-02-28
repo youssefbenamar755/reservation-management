d386ea1ebe322b0baa9351b622a88d7cc54dbe64
Youssef benamar <youssefbenamar755@gmail.com@gmail.com>
2026-02-28 23:10:53 +0000

    Added Http facade import in WebsiteController for enhanced HTTP functionality

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -6,6 +6,7 @@ use App\Models\Website;
 use App\Jobs\SyncFluentSubmissions;
 use App\Services\WooCommerceOrderSyncService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
